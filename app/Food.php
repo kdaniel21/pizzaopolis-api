927b868a9a9d33ad0ad286c4b6e103ad3b64d605
Daniel Kiss <kissd621@gmail.com>
2020-09-16 10:56:22 +0200

    Removed unused code

--- app/Food.php
+++ app/Food.php
@@ -15,16 +15,4 @@ class Food extends Model {
     public function currentPrice() {
         return $this->discounted_price ?: $this->price;
     }
-
-    // public function toArray() {
-    //     $attributes = $this->attributesToArray();
-    //     $attributes = array_merge($attributes, $this->relationsToArray());
-
-    //     // Add the quantity from the pivot table and hide the pivot
-    //     if (isset($attributes['pivot']['quantity'])) {
-    //         $attributes['quantity'] = $attributes['pivot']['quantity'];
-    //         // unset($attributes['pivot']);
-    //     }
-    //     return $attributes;
-    // }
 }
