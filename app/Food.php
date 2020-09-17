d970f8b6a3228ba4cfbba4a91e048add87ce2f5e
Daniel Kiss <kissd621@gmail.com>
2020-09-17 11:12:58 +0200

    Included categories

--- app/Food.php
+++ app/Food.php
@@ -3,13 +3,20 @@
 namespace App;
 
 use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
+use App\Ingredient;
+use App\Category;
 
 class Food extends Model {
     protected $guarded = [];
     protected $hidden = ['active', 'created_at', 'updated_at'];
+    protected $with = ['ingredients', 'categories'];
     
     public function ingredients() {
         return $this->belongsToMany(Ingredient::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
     }
 
     public function currentPrice() {
